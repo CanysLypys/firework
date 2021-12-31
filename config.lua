8a2d54a3e7e3c8caca1917cd8967bac628cf9610
Canis_Lupus <56067968+CanysLypys@users.noreply.github.com>
2021-12-31 17:30:40 +0100

    Update config.lua
    Changed from German into English.

--- config.lua
+++ config.lua
@@ -1,17 +1,17 @@
 Config = {}
 
-Config.FeuerwerkOrte = {
-    { ["x"] = -1934.69, ["y"] = -1333.48, ["z"] = 20.74, ["Feuerwerktyp"] = "Batterie"},
-    { ["x"] = -1800.69, ["y"] = -1333.48, ["z"] = 20.74, ["Feuerwerktyp"] = "Rakete"},
-    { ["x"] = -1752.13, ["y"] = -1486.68, ["z"] = 20.74, ["Feuerwerktyp"] = "Fontäne"},
-    { ["x"] = -1896.52, ["y"] = -1491.06, ["z"] = 20.74, ["Feuerwerktyp"] = "Rakete"},
-    { ["x"] = -1714.08, ["y"] = -1404.71, ["z"] = 20.74, ["Feuerwerktyp"] = "Batterie"},
-    { ["x"] = -1635.27, ["y"] = -1574.06, ["z"] = 20.74, ["Feuerwerktyp"] = "Fontäne"},
-    { ["x"] = -1714.08, ["y"] = -1404.71, ["z"] = 20.74, ["Feuerwerktyp"] = "Batterie"},
-    { ["x"] = -1661.6, ["y"] = -1477.31, ["z"] = 20.74, ["Feuerwerktyp"] = "Rakete"},
-    { ["x"] = -1784.64, ["y"] = -1309.06, ["z"] = 20.74, ["Feuerwerktyp"] = "Fontäne"},
-    { ["x"] = -1778.74, ["y"] = -1399.71, ["z"] = 20.74, ["Feuerwerktyp"] = "Rakete"},
-    { ["x"] = -1826.71, ["y"] = -1523.61, ["z"] = 20.74, ["Feuerwerktyp"] = "Fontäne"},
-    { ["x"] = -1766.11, ["y"] = -1638.99, ["z"] = 20.74, ["Feuerwerktyp"] = "Rakete"},
-    { ["x"] = -1687.31, ["y"] = -1557.52, ["z"] = 20.74, ["Feuerwerktyp"] = "Batterie"},
+Config.FireworkLocations = {
+    { ["x"] = -1934.69, ["y"] = -1333.48, ["z"] = 20.74, ["Fireworktype"] = "Battery"},
+    { ["x"] = -1800.69, ["y"] = -1333.48, ["z"] = 20.74, ["Fireworktype"] = "Rocket"},
+    { ["x"] = -1752.13, ["y"] = -1486.68, ["z"] = 20.74, ["Fireworktype"] = "Fountain"},
+    { ["x"] = -1896.52, ["y"] = -1491.06, ["z"] = 20.74, ["Fireworktype"] = "Rocket"},
+    { ["x"] = -1714.08, ["y"] = -1404.71, ["z"] = 20.74, ["Fireworktype"] = "Battery"},
+    { ["x"] = -1635.27, ["y"] = -1574.06, ["z"] = 20.74, ["Fireworktype"] = "Fountain"},
+    { ["x"] = -1714.08, ["y"] = -1404.71, ["z"] = 20.74, ["Fireworktype"] = "Battery"},
+    { ["x"] = -1661.6, ["y"] = -1477.31, ["z"] = 20.74, ["Fireworktype"] = "Rocket"},
+    { ["x"] = -1784.64, ["y"] = -1309.06, ["z"] = 20.74, ["Fireworktype"] = "Fountain"},
+    { ["x"] = -1778.74, ["y"] = -1399.71, ["z"] = 20.74, ["Fireworktype"] = "Rocket"},
+    { ["x"] = -1826.71, ["y"] = -1523.61, ["z"] = 20.74, ["Fireworktype"] = "Fountain"},
+    { ["x"] = -1766.11, ["y"] = -1638.99, ["z"] = 20.74, ["Fireworktype"] = "Rocket"},
+    { ["x"] = -1687.31, ["y"] = -1557.52, ["z"] = 20.74, ["Fireworktype"] = "Battery"},
 }
