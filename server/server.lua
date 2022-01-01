88aec25cb7c1545e53c934268bab8c222ec308a8
Canis_Lupus <56067968+CanysLypys@users.noreply.github.com>
2022-01-01 12:33:42 +0100

    Reliable Network Event Overflow fixed.

--- server/server.lua
+++ server/server.lua
@@ -7,9 +7,6 @@ GetTime = function()
 	return {d = d, h = h, m = m}
 end
 
-local fireworkOver = false
-
-
 CreateThread(function()
     while true do
         Wait(0)
@@ -26,8 +23,8 @@ CreateThread(function()
         Wait(0)
         time = GetTime()
         if time.h == 0 and time.m == 15 then
-            fireworkOver = true
             print("^1Fireworkshow is over.^0")
+            TriggerClientEvent("firework:stopFireworkShow", -1)
             break
         end
     end
@@ -39,33 +36,22 @@ FireworkShow = function()
 end
 
 RegisterCommand("fireworkshow", function(source)
-    print("^Fireworkshow started manually.^0")
+    print("^2Fireworkshow started manually.^0")
     TriggerClientEvent("firework:startFireworkShow", source)
 end)
 
 RegisterNetEvent("firework:battery")
 AddEventHandler("firework:battery", function(fireworkPos)
-   if fireworkOver then
-        return
-    else
-        TriggerClientEvent("firework:battery", -1, fireworkPos)
-    end
+    TriggerClientEvent("firework:battery", -1, fireworkPos)
 end)
 
 RegisterNetEvent("firework:rocket")
 AddEventHandler("firework:rocket", function(fireworkPos)
-   if fireworkOver then
-        return
-    else
-        TriggerClientEvent("firework:rocket", -1, fireworkPos)
-    end
+    TriggerClientEvent("firework:rocket", -1, fireworkPos)
 end)
+
 
 RegisterNetEvent("firework:fountain")
 AddEventHandler("firework:fountain", function(fireworkPos)
-    if fireworkOver then
-        return
-    else
-        TriggerClientEvent("firework:fountain", -1, fireworkPos)
-    end
+    TriggerClientEvent("firework:fountain", -1, fireworkPos)
 end)
