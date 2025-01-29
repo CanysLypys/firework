ebd3ab026c62d406b823c2a8c01b32e79d04b89d
Canis <56067968+CanysLypys@users.noreply.github.com>
2025-01-29 17:23:31 +0100

    Performance Update

--- server/server.lua
+++ server/server.lua
@@ -1,57 +1,36 @@
-GetTime = function()
-	local timestamp = os.time()
-	local d = os.date("*t", timestamp).wday
-	local h = tonumber(os.date("%H", timestamp))
-	local m = tonumber(os.date("%M", timestamp))
+local function GetTime()
+    local timestamp = os.time()
+    local d = os.date("*t", timestamp).wday
+    local h = tonumber(os.date("%H", timestamp))
+    local m = tonumber(os.date("%M", timestamp))
 
-	return {d = d, h = h, m = m}
+    return {d = d, h = h, m = m}
 end
 
 CreateThread(function()
     while true do
-        Wait(0)
-        time = GetTime()
+        Wait(60000)
+        local time = GetTime()
         if time.h == 0 and time.m == 0 then
             FireworkShow()
-            break
-        end
-    end
-end)
-
-CreateThread(function()
-    while true do
-        Wait(0)
-        time = GetTime()
-        if time.h == 0 and time.m == 15 then
+        elseif time.h == 0 and time.m == 15 then
             print("^1Fireworkshow is over.^0")
             TriggerClientEvent("firework:stopFireworkShow", -1)
-            break
         end
     end
 end)
 
-FireworkShow = function()
+local function FireworkShow()
     print("It's new year! Time to shine. Starting Fireworkshow....")
     TriggerClientEvent("firework:startFireworkShow", -1)
 end
 
 RegisterCommand("fireworkshow", function(source)
     print("^2Fireworkshow started manually.^0")
-    TriggerClientEvent("firework:startFireworkShow", source)
+    TriggerClientEvent("firework:startFireworkShow", -1)
 end)
 
-RegisterNetEvent("firework:battery")
-AddEventHandler("firework:battery", function(fireworkPos)
-    TriggerClientEvent("firework:battery", -1, fireworkPos)
-end)
-
-RegisterNetEvent("firework:rocket")
-AddEventHandler("firework:rocket", function(fireworkPos)
-    TriggerClientEvent("firework:rocket", -1, fireworkPos)
-end)
-
-
-RegisterNetEvent("firework:fountain")
-AddEventHandler("firework:fountain", function(fireworkPos)
-    TriggerClientEvent("firework:fountain", -1, fireworkPos)
+RegisterNetEvent("firework:launch")
+AddEventHandler("firework:launch", function(fireworkPos, fireworkType)
+    TriggerClientEvent("firework:launch", -1, fireworkPos, fireworkType)
 end)
